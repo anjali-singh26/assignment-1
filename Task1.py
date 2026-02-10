5266eb222f49ef00034cff80c048d5036405544c
Anjali Singh <148558504+anjali-singh26@users.noreply.github.com>
2026-02-10 20:21:53 +0530

    Create Task1.py

---
+++ Task1.py
@@ -0,0 +1,12 @@
+# Task1
+n1=int(input("Enter the First number :"))
+n2=int(input("Enter the Second number :"))
+
+n=n1+n2
+print("Addition :",n)
+n=n1-n2
+print("Subtraction :",n)
+n=n1*n2
+print("Multiplication :",n)
+n=n1/n2
+print("Division :",n)
